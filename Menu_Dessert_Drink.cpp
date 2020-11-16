89095b3d40585b70bed0ab1ca83cd1ede5471a84
Louis <louis27isnow@gmail.com>
2020-11-16 13:01:48 +0700

    added file, orderFood function

--- Menu_Dessert_Drink.cpp
+++ Menu_Dessert_Drink.cpp
@@ -1,4 +1,3 @@
-
 void createDessert(char *name, int price, char *topping, double calories, int time, int n)
 {
 	strcpy(Food[n].type, "Dessert");
@@ -28,21 +27,21 @@ void createDrink(char *name, int price, char *flavor, char size, int time, int n
 void addFood()
 {
 	int input;
-	system("cls");
+	system("cls || clear");
 	puts("What do you want to add?");
 	puts("1. Dessert");
 	puts("2. Drink");
 	do
 	{
-	printf("Choose: ");
-	scanf("%d", &input);	
-	switch(input)
-	{
+		printf("Choose: ");
+		scanf("%d", &input);
+		switch (input)
+		{
 		case 1:
-		dessertMenu();
-		break;
+			dessertMenu();
+			break;
 		case 2:
-                drinkMenu();
+			drinkMenu();
 		}
 	} while (input != 1 && input != 2);
 }
@@ -52,35 +51,35 @@ void dessertMenu()
 	char nameTemp[100], toppingTemp[100];
 	int priceTemp, extraTime, timeTemp;
 	double caloriesTemp;
-	
+
 	do
 	{
-	printf("Input the name [at least 5 characters]: ");
-	getchar();
-        scanf("%[^\n]", nameTemp);
-        }while(strlen(nameTemp) < 5);
-    
-        do
-	{
-        printf("Input the price [10 - 500]: $ ");
-        scanf("%d", &priceTemp);
-	}while(priceTemp < 10 || priceTemp > 500);
-    
+		printf("Input the name [at least 5 characters]: ");
+		getchar();
+		scanf("%[^\n]", nameTemp);
+	} while (strlen(nameTemp) < 5);
+
 	do
 	{
-	printf("Input the topping ['Caramel' | 'Honey' | 'Syrup'](Case Insensitive): ");
-	getchar();
-	scanf("%[^\n]", toppingTemp);
-	extraTime = strcmpInsensitive(toppingTemp);
-	}while(extraTime == -1);
+		printf("Input the price [10 - 500]: $ ");
+		scanf("%d", &priceTemp);
+	} while (priceTemp < 10 || priceTemp > 500);
+
+	do
+	{
+		printf("Input the topping ['Caramel' | 'Honey' | 'Syrup'](Case Insensitive): ");
+		getchar();
+		scanf("%[^\n]", toppingTemp);
+		extraTime = strcmpInsensitive(toppingTemp);
+	} while (extraTime == -1);
 	strcpy(toppingTemp, defstring(toppingTemp));
-	
+
 	do
 	{
-	printf("Insert calories [1.00 - 99.00]: ");
-	scanf("%lf", &caloriesTemp);
-	}while(caloriesTemp < 1.00 || caloriesTemp > 99.00);
-	
+		printf("Insert calories [1.00 - 99.00]: ");
+		scanf("%lf", &caloriesTemp);
+	} while (caloriesTemp < 1.00 || caloriesTemp > 99.00);
+
 	timeTemp = cookingTimeDessert(extraTime);
 	createDessert(nameTemp, priceTemp, toppingTemp, caloriesTemp, timeTemp, n);
 	printf("\nSuccessfully added a new menu!");
@@ -93,34 +92,34 @@ void drinkMenu()
 {
 	char nameTemp[100], flavorTemp[100], sizeTemp;
 	int priceTemp, extraTime, timeTemp;
-	
+
 	do
 	{
-	printf("Input the name [at least 5 characters]: ");
-	getchar();
-	scanf("%[^\n]", nameTemp);
-	}while(strlen(nameTemp) < 5);
-	
+		printf("Input the name [at least 5 characters]: ");
+		getchar();
+		scanf("%[^\n]", nameTemp);
+	} while (strlen(nameTemp) < 5);
+
 	do
 	{
-	printf("Input the price [10 - 500]: $ ");
-	scanf("%d", &priceTemp);
-	}while(priceTemp < 10 || priceTemp > 500);
-	
+		printf("Input the price [10 - 500]: $ ");
+		scanf("%d", &priceTemp);
+	} while (priceTemp < 10 || priceTemp > 500);
+
 	do
 	{
-	printf("Input the flavor ['Mint' | 'Mix Berry' | 'Cheese'](Case Sensitive): ");
-	getchar();
-	scanf("%[^\n]", flavorTemp);
-	}while(strcmp(flavorTemp, "Mint") != 0 && strcmp(flavorTemp, "Mix Berry") != 0 && strcmp(flavorTemp, "Cheese") != 0);
-	
+		printf("Input the flavor ['Mint' | 'Mix Berry' | 'Cheese'](Case Sensitive): ");
+		getchar();
+		scanf("%[^\n]", flavorTemp);
+	} while (strcmp(flavorTemp, "Mint") != 0 && strcmp(flavorTemp, "Mix Berry") != 0 && strcmp(flavorTemp, "Cheese") != 0);
+
 	do
 	{
-	printf("Input the size [S | M | L](Case Sensitive): ");
-	getchar();
-	scanf("%c", &sizeTemp);
-	}while(sizeTemp != 'S' && sizeTemp != 'M' && sizeTemp != 'L');
-	
+		printf("Input the size [S | M | L](Case Sensitive): ");
+		getchar();
+		scanf("%c", &sizeTemp);
+	} while (sizeTemp != 'S' && sizeTemp != 'M' && sizeTemp != 'L');
+
 	timeTemp = cookingTimeDrink(flavorTemp);
 	printf("\nSuccessfully added a new menu!");
 	createDrink(nameTemp, priceTemp, flavorTemp, sizeTemp, timeTemp, n);
@@ -131,22 +130,22 @@ void drinkMenu()
 
 int strcmpInsensitive(char topping[])
 {
-	for(int i = 0;topping[i] != '\0';i++)
+	for (int i = 0; topping[i] != '\0'; i++)
 	{
-		if(topping[i] >= 'A' && topping[i] <= 'Z')
+		if (topping[i] >= 'A' && topping[i] <= 'Z')
 		{
 			topping[i] += 32;
 		}
 	}
-	if(strcmp(topping, "caramel") == 0)
+	if (strcmp(topping, "caramel") == 0)
 	{
 		return 1;
 	}
-	else if(strcmp(topping, "honey") == 0)
+	else if (strcmp(topping, "honey") == 0)
 	{
 		return 2;
 	}
-	else if(strcmp(topping, "syrup") == 0)
+	else if (strcmp(topping, "syrup") == 0)
 	{
 		return 3;
 	}
@@ -158,11 +157,11 @@ int strcmpInsensitive(char topping[])
 int cookingTimeDessert(int extra)
 {
 	int time = 0;
-	if(extra == 1)
+	if (extra == 1)
 	{
 		time += 10;
 	}
-	else if(extra == 2)
+	else if (extra == 2)
 	{
 		time += 15;
 	}
@@ -177,11 +176,11 @@ int cookingTimeDessert(int extra)
 int cookingTimeDrink(char flavor[])
 {
 	int time = 0;
-	if(strcmp(flavor, "Mint") == 0)
+	if (strcmp(flavor, "Mint") == 0)
 	{
 		time += 10;
 	}
-	else if(strcmp(flavor, "Mix Berry") == 0)
+	else if (strcmp(flavor, "Mix Berry") == 0)
 	{
 		time += 20;
 	}
@@ -195,13 +194,13 @@ int cookingTimeDrink(char flavor[])
 
 char *defstring(char str[])
 {
-	for(int i = 0;str[i] != '\0';i++)
+	for (int i = 0; str[i] != '\0'; i++)
 	{
-		if(i == 0 && str[i] >= 'a' && str[i] <= 'z')
+		if (i == 0 && str[i] >= 'a' && str[i] <= 'z')
 		{
 			str[i] -= 32;
 		}
-		else if(str[i] >= 'A' && str[i] <= 'Z')
+		else if (str[i] >= 'A' && str[i] <= 'Z')
 		{
 			str[i] += 32;
 		}
@@ -211,6 +210,6 @@ char *defstring(char str[])
 
 void sleep()
 {
-	for(long long int i = 0;i < 1e9;i++);
+	for (long long int i = 0; i < 1e9; i++)
+		;
 }
-
