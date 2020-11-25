8527ced96c94e5c72002a1b139acbcfc89b68aed
kevin christanto <kevin.christanto@binus.ac.id>
2020-11-25 18:43:23 +0700

    changed curr -> curr2

--- orderHistory.cpp
+++ orderHistory.cpp
@@ -13,12 +13,12 @@ void view_order_history()
         system("cls || clear");
         printf("| %-3s | %-20s | %-6s | %-10s | %-10s | %-10s | %-5s | %-20s |\n", "No", "Name", "Price", "Topping", "Calories", "Flavor", "Size", "Order Time");
         puts("-------------------------------------------------------------------------------------------------------------");
-        curr = head;
+        curr2 = head2;
         int i = 1;
-        while (curr)
+        while (curr2)
         {
             printf("| %-3d | %-20s | %-6d | %-10s | %-10.2lf | %-10s | %-5c | %-30s |\n", i, curr2->Orders.Food.name, curr2->Orders.Food.price, curr2->Orders.Food.topping, curr2->Orders.Food.calories, curr2->Orders.Food.flavor, curr2->Orders.Food.size, curr2->Orders.orderedtime);
-            curr = curr->next;
+            curr2 = curr2->next;
             i++;
         }
         getchar();
